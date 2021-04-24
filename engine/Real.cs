66d56171675c0f6ddc247a0d3a50b12b059c42f4
adamnorberg <norberg@google.com>
2021-04-24 13:10:40 -0700

    Also include Decimal.

--- engine/Real.cs
+++ engine/Real.cs
@@ -28,17 +28,41 @@ namespace FreedomOfFormFoundation.AnatomyEngine
         /// </summary>
         private double _v;
 
+        /// <summary>
+        /// Construct a Real with the value provided.
+        /// </summary>
+        /// <param name="v">Value this Real should take.</param>
         public Real(double v)
         {
             _v = v;
         }
 
-        // Real permits automatic casts <i>from</i> double and float. It will not automatically cast to them, since
-        // this would prevent the compiler from spotting math being done with native numeric types that needs to
-        // be converted to Real.
+        /// <summary>
+        /// Construct a Real with the value provided.
+        /// </summary>
+        /// <param name="v">Value this Real should take.</param>
+        public Real(float v)
+        {
+            _v = v;
+        }
+
+        /// <summary>
+        /// Construct a Real with the value provided.
+        /// </summary>
+        /// <param name="v">Value this Real should take.</param>
+        public Real(decimal v)
+        {
+            _v = v;
+        }
+
+        // Real permits automatic casts <i>from</i> double, float, and decimal. It will not
+        // automatically cast to them, since this would prevent the compiler from spotting math
+        // being done with native numeric types that needs to be converted to Real.
         public static implicit operator Real(double d) => new Real(d);
         public static implicit operator Real(float f) => new Real(f);
+        public static implicit operator Real(decimal m) => new Real(m);
         public static explicit operator double(Real r) => (double)r._v;
         public static explicit operator float(Real r) => (float)r._v;
+        public static explicit operator decimal(Real r) => (decimal) r._v;
     }
 }
