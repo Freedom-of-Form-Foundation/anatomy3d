73ccb27130995c5d3bb590389e8c96cab2ee3e0a
Lathreas <lathreas@protonmail.com>
2021-05-04 03:22:57 +0200

    Allow a LongBone to be defined using a 1D radius function
    This uses the new DomainToVector2<O> class to convert a 1D
    function to a 2D function with one constant axis.

--- engine/anatomy/bones/LongBone.cs
+++ engine/anatomy/bones/LongBone.cs
@@ -7,13 +7,25 @@ namespace FreedomOfFormFoundation.AnatomyEngine.Anatomy.Bones
 {
 	public class LongBone : Bone
 	{
-		ContinuousMap<float, float> Radius { get; set; }
+		ContinuousMap<Vector2, float> Radius { get; set; }
 		Curve CenterCurve { get; set; }
 		
-		public LongBone(Curve centerCurve, ContinuousMap<float, float> radius)
+		public LongBone(Curve centerCurve, ContinuousMap<Vector2, float> radius)
 		{
 			this.CenterCurve = centerCurve;
 			this.Radius = radius;
+		}
+		
+		public LongBone(Curve centerCurve, ContinuousMap<float, float> radius)
+			: this(centerCurve, new DomainToVector2<float>(new Vector2(0.0f, 1.0f), radius))
+		{
+			
+		}
+		
+		public LongBone(Curve centerCurve, float radius)
+			: this(centerCurve, new ConstantFunction<Vector2, float>(radius))
+		{
+			
 		}
 		
 		/// <summary>
