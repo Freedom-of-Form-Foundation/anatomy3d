025c0c474ae420a07acb063d39c488e5d861d7e6
Lathreas <lathreas@protonmail.com>
2021-05-04 03:44:34 +0200

    Rewrite Hemisphere.cs to prepare for a 2D height map
    This also makes the code more elegant and reduces the use of
    expensive matrices to calculate vertex positions.

--- engine/geometry/Hemisphere.cs
+++ engine/geometry/Hemisphere.cs
@@ -1,19 +1,20 @@
 using System.Collections.Generic;
 using System.Numerics;
 using System;
+using FreedomOfFormFoundation.AnatomyEngine.Calculus;
 
 namespace FreedomOfFormFoundation.AnatomyEngine.Geometry
 {
 	public class Hemisphere : Surface
 	{
-		public Hemisphere(float radius, Vector3 center, Vector3 direction)
+		public Hemisphere(ContinuousMap<Vector2, float> radius, Vector3 center, Vector3 direction)
 		{
 			this.Radius = radius;
 			this.Center = center;
 			this.Direction = direction;
 		}
 		
-		float Radius { get; set; }
+		ContinuousMap<Vector2, float> Radius { get; set; }
 		
 		Vector3 Center { get; set; }
 		
@@ -34,43 +35,51 @@ namespace FreedomOfFormFoundation.AnatomyEngine.Geometry
 			return resolutionU * resolutionV * 6 + resolutionU * 3;
 		}
 		
+		public Vector3 GetNormalAt(Vector2 uv)
+		{
+			float u = uv.X;
+			float v = uv.Y;
+			
+			Vector3 up = new Vector3(0.0f, 0.0f, 1.0f);
+			
+			Vector3 pointTangent = this.direction;
+			Vector3 pointNormal = Vector3.Normalize(Vector3.Cross(pointTangent, up));
+			Vector3 pointBinormal = Vector3.Normalize(Vector3.Cross(pointTangent, pointNormal));
+			
+			// Calculate the position of the rings of vertices:
+			float x = (float)Math.Sin(v) * (float)Math.Cos(u);
+			float y = (float)Math.Sin(v) * (float)Math.Sin(u);
+			float z = (float)Math.Cos(v);
+			
+			return x*pointNormal + y*pointBinormal + z*pointTangent;
+		}
+		
+		public Vector3 GetPositionAt(Vector2 uv)
+		{
+			Vector3 translation = Center;
+			float radius = this.Radius.GetValueAt(uv);
+			
+			return translation + radius*GetNormalAt(uv);
+		}
+		
 		public override List<Vertex> GenerateVertexList(int resolutionU, int resolutionV)
 		{
 			List<Vertex> output = new List<Vertex>(CalculateVertexCount(resolutionU, resolutionV));
 			
+			// Load all required variables:
 			Vector3 up = new Vector3(0.0f, 0.0f, 1.0f);
-			float sign = 1.0f;
 			
-			// To prevent division by zero, flip the direction if facing the other way:
-			if(Vector3.Dot(up, this.direction) < 0.0f)
-			{
-				sign = -1.0f;
-			}
+			Vector3 pointTangent = this.direction;
+			Vector3 pointNormal = Vector3.Normalize(Vector3.Cross(pointTangent, up));
+			Vector3 pointBinormal = Vector3.Normalize(Vector3.Cross(pointTangent, pointNormal));
 			
-			// Generate a rotation matrix which rotates the 'up' vector onto the
-			// 'direction' vector, using Rodrigues' Rotation Formula:
-			Vector3 k = Vector3.Cross(sign*this.direction, up);
+			Vector3 translation = Center;
 			
-			Matrix4x4 identity = new Matrix4x4(1.0f, 0.0f, 0.0f, 0.0f,
-											   0.0f, 1.0f, 0.0f, 0.0f,
-											   0.0f, 0.0f, 1.0f, 0.0f,
-											   0.0f, 0.0f, 0.0f, 1.0f);
-			
-			Matrix4x4 K = new Matrix4x4(0.0f, -k.Z,  k.Y, 0.0f,
-										 k.Z, 0.0f, -k.X, 0.0f,
-										-k.Y,  k.X, 0.0f, 0.0f,
-										0.0f, 0.0f, 0.0f, 0.0f);
-			
-			Matrix4x4 rotationMatrix = identity + K + K*K*(1.0f/(1.0f + Vector3.Dot(up, sign*this.direction)));
-			
-			Matrix4x4 transformationMatrix = rotationMatrix;
-			transformationMatrix.Translation = Center;
-			
-			float radius = this.Radius;
+			// Get the radius at the top of the hemisphere:
+			float topRadius = this.Radius.GetValueAt(new Vector2(0.0f, 0.5f*(float)Math.PI));
 
 			// Generate the first point at the pole of the hemisphere:
-			output.Add(new Vertex(Vector3.Transform(sign*radius*up, transformationMatrix),
-								  Vector3.Transform(sign*up, rotationMatrix)));
+			output.Add(new Vertex(translation + topRadius*pointTangent, pointTangent));
 			
 			// Generate rings of the other points:
 			for (int j = 1; j < (resolutionV+1); j++)
@@ -82,17 +91,16 @@ namespace FreedomOfFormFoundation.AnatomyEngine.Geometry
 					float v = (float)j/(float)resolutionV * 0.5f * (float)Math.PI;
 					
 					// Calculate the position of the rings of vertices:
-					float x = (float)Math.Sin(v) * (float)Math.Cos(sign*u);
-					float y = (float)Math.Sin(v) * (float)Math.Sin(sign*u);
+					float x = (float)Math.Sin(v) * (float)Math.Cos(u);
+					float y = (float)Math.Sin(v) * (float)Math.Sin(u);
 					float z = (float)Math.Cos(v);
 					
-					Vector3 position = new Vector3(x, y, z);
+					float radius = this.Radius.GetValueAt(new Vector2(u, v));
 					
-					// Rotate the vector to orient the hemisphere correctly:
-					Vector3 vertexPosition = Vector3.Transform(sign*radius * position, transformationMatrix);
-					Vector3 normal = Vector3.Transform(sign*position, rotationMatrix);
+					Vector3 surfaceNormal = x*pointNormal + y*pointBinormal + z*pointTangent;
+					Vector3 surfacePosition = translation + radius*surfaceNormal;
 					
-					output.Add(new Vertex(vertexPosition, normal));
+					output.Add(new Vertex(surfacePosition, surfaceNormal));
 				}
 			}
 			
