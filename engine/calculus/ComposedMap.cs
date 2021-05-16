1ce6a3405e1152dde827564ba88d8dd1acf79d06
Adam Norberg <norberg@google.com>
2021-05-16 14:35:28 -0700

    ComposedMap: f ∘ g for ContinuousMaps, basically
    Also adds ContinuousMapExtensions.cs, providing fake member functions for ContinuousMap (for functions that don't need any inside details on the maps). Note that extensions will continue to work if we refactor ContinuousMap to IContinousMap, whereas member functions with bodies do not.
    
    The constructor for ComposedMap is ugly because new ComposedMap<in, intermediate, out> is long to type, but automatic type deduction for method calls means the convenience methods in ContinuousMapExtensions.cs do not need explicit type parameters, so use them instead.

---
+++ engine/calculus/ComposedMap.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace FreedomOfFormFoundation.AnatomyEngine.Calculus
+{
+	// ComposedMap represents one mapping function composed with another, such that the left map "feeds into" the
+	// right map. This map behaves like right(left(x)). It's intended to be constructed by the convenience
+	// extension method "Then" - see ContinuousMapExtensions.cs for details - where automatic type deduction in
+	// method calls will get rid of the type boilerplate.
+	public sealed class ComposedMap<TIn, TIntermediate, TOut> : ContinuousMap<TIn, TOut>
+	{
+		/// <summary>
+		/// First map to use, calculating a final value.
+		/// </summary>
+		private readonly ContinuousMap<TIn, TIntermediate> _left;
+
+		/// <summary>
+		/// Second map to use, calculating a final value from the intermediate value.
+		/// </summary>
+		private readonly ContinuousMap<TIntermediate, TOut> _right;
+
+		/// <summary>
+		/// Constructs a ComposedMap feeding the left map into the right map to produce a map from left.I to right.O.
+		/// Due to the general ugliness of ComposedMap's full type name, you probably don't want to use this directly.
+		/// Use left.Then(right) instead.
+		/// </summary>
+		/// <param name="left">Map from input value to an intermediate value. Nonnull.</param>
+		/// <param name="right">Map from intermediate value to the final value. Nonnull.</param>
+		/// <exception cref="ArgumentException">If either argument is null.</exception>
+		public ComposedMap(ContinuousMap<TIn, TIntermediate> left,
+			ContinuousMap<TIntermediate, TOut> right)
+		{
+			if (left == null)
+			{
+				throw new ArgumentException("ComposedMap constructor requires nonnull arguments", "left");
+			}
+
+			if (right == null)
+			{
+				throw new ArgumentException("ComposedMap constructor requires nonnull arguments", "right");
+			}
+
+			_left = left;
+			_right = right;
+		}
+
+		/// <summary>
+		/// Gets a value from the second continuous map this object was constructed with by giving it the output of
+		/// the first map, which is given the input to this function. Composes functions.
+		/// </summary>
+		/// <param name="t">Value to give to first map.</param>
+		/// <returns>Value of second map at the first map's value for t.</returns>
+		public override TOut GetValueAt(TIn t) => _right.GetValueAt(_left.GetValueAt(t));
+	}
+}
