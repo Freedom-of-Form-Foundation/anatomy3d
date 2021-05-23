0dae83779d0276317949ebc37b29fcae9b9a7a95
Adam Norberg <norberg@google.com>
2021-05-22 23:25:43 -0700

    Add copyright notice, implement test for Addition. It passes.

--- EngineTests/RealTests.cs
+++ EngineTests/RealTests.cs
@@ -1,14 +1,35 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021 Freedom of Form Foundation, Inc.
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License, version 2 (GPLv2) as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License, version 2 (GPLv2) for more details.
+ * 
+ * You should have received a copy of the GNU General Public License, version 2 (GPLv2)
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+using System;
 using Xunit;
+using FreedomOfFormFoundation.AnatomyEngine;
 
 namespace EngineTests
 {
     public class Tests
     {
         [Fact]
-        public void Test1()
+        public void TestAddition()
         {
-            Assert.True(true);
+            Assert.Equal(new Real(2) + new Real(2), new Real(4));
+            Assert.Equal(new Real(2) + new Real(-2), new Real(0));
+            Assert.Equal(new Real(-1000000.0) + Real.PositiveInfinity, Real.PositiveInfinity);
+            Assert.Equal(Real.PositiveInfinity + Real.NegativeInfinity, Real.NaN);
+            Assert.Equal(new Real(9999999) + Real.NegativeInfinity, Real.NegativeInfinity);
         }
     }
 }
