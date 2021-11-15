2c3eab6bd18b0940f47c9fc85732df37e16217b7
Adam Norberg <norberg@google.com>
2021-11-14 16:31:20 -0800

    Fix unit tests.

--- EngineTests/calculus/QuadraticFunctionTests.cs
+++ EngineTests/calculus/QuadraticFunctionTests.cs
@@ -28,43 +28,19 @@ namespace EngineTests.calculus
         /// <param name="x">Value to test at.</param>
         /// <param name="expected"></param>
         [Theory]
-        [InlineData(0f, 0f, 0f, 0f, 0f)]
-        [InlineData(1000f, 0f, 0f, 0f, 1000f)]
-        [InlineData(1000f, 0f, 0f, 87429f, 1000f)]
-        [InlineData(0f, 1f, 0f, 0f, 0f)]
-        [InlineData(0f, 1f, 0f, 3.14159f, 3.14159f)]
-        [InlineData(0f, 1e10f, 0f, 1e5f, 1e15f)]
-        [InlineData(42f, 20f, 0f, 10f, 242f)]
-        [InlineData(float.NegativeInfinity, 1f, 0f, 10f, float.NegativeInfinity)]
-        [InlineData(float.PositiveInfinity, 1e-15f, 0f, float.PositiveInfinity, float.PositiveInfinity)]
-        [InlineData(1000f, 0f, 0f, float.NegativeInfinity, float.NegativeInfinity)]
-        public void TestValueCalculation(float a0, float a1, float a2, float x, float expected)
+        [InlineData(0, 0, 0, 0, 0)]
+        [InlineData(1000, 0, 0, 0, 1000)]
+        [InlineData(1000, 0, 0, 87429, 1000)]
+        [InlineData(0, 1, 0, 0, 0)]
+        [InlineData(0, 1, 0, 3.14159, 3.14159)]
+        [InlineData(0, 1e10, 0, 1e5, 1e15)]
+        [InlineData(42, 20, 0, 10, 242)]
+        public void TestValueCalculation(double a0, double a1, double a2, double x, double expected)
         {
             QuadraticFunction q = new QuadraticFunction(a0, a1, a2);
             Assert.NotNull(q);
             Assert.Equal(expected, q.GetValueAt(x), 6);
             Assert.Equal(expected, q.GetNthDerivativeAt(x, 0), 6);
-        }
-
-        /// <summary>
-        /// Tests quadratic function evaluation in cases where the expected value is NaN, which
-        /// requires special comparison.
-        ///
-        /// Note that, in IEEE floating point, 0 is "zero-ish", so multiplying infinity by a zero is numerically
-        /// undefined, represented as NaN. Cases multiplying infinity by zero wind up here.
-        /// </summary>
-        /// <param name="a0">Constant term in quadratic function.</param>
-        /// <param name="a1">Linear term in quadratic function.</param>
-        /// <param name="a2">Quadratic term in quadratic function.</param>
-        /// <param name="x">Value to test at.</param>
-        [Theory]
-        [InlineData(float.PositiveInfinity, 1e-15f, 0f, float.NegativeInfinity)]
-        public void TestValueNaN(float a0, float a1, float a2, float x)
-        {
-            QuadraticFunction q = new QuadraticFunction(a0, a1, a2);
-            Assert.NotNull(q);
-            Assert.True(float.IsNaN(q.GetValueAt(x)));
-            Assert.True(float.IsNaN(q.GetNthDerivativeAt(x, 0)));
         }
 
         /// <summary>
@@ -80,11 +56,11 @@ namespace EngineTests.calculus
         /// <param name="atD1">Expected value of the first derivative at x.</param>
         /// <param name="atD2">Expected value of the second derivative at x.</param>
         [Theory]
-        [InlineData(0f,0f,0f,0f,0f,0f,0f)]
-        [InlineData(1f, 0f, 0f, 20f, 1f, 0f, 0f)]
-        [InlineData(1f, 2f, 0f, 10f, 21f, 2f, 0f)]
-        [InlineData(1f, 2f, 4f, 2f, 21f, 18f, 8f)]
-        public void TestDerivativesAt(float a0, float a1, float a2, float x, float atD0, float atD1, float atD2)
+        [InlineData(0, 0, 0, 0, 0, 0, 0)]
+        [InlineData(1, 0, 0, 20, 1, 0, 0)]
+        [InlineData(1, 2, 0, 10, 21, 2, 0)]
+        [InlineData(1, 2, 4, 2, 21, 18, 8)]
+        public void TestDerivativesAt(double a0, double a1, double a2, double x, double atD0, double atD1, double atD2)
         {
             QuadraticFunction q = new QuadraticFunction(a0, a1, a2);
             Assert.NotNull(q);
@@ -98,10 +74,10 @@ namespace EngineTests.calculus
 
 
         [Theory]
-        [InlineData(0f, 1f, 1f, new float[]{-1f, 0f})]
-        public void TestSolve(float a0, float a1, float a2, float[] want)
+        [InlineData(0, 1, 1, new double[]{-1, 0})]
+        public void TestSolve(double a0, double a1, double a2, double[] want)
         {
-            IList<float> got = QuadraticFunction.Solve(a0, a1, a2).ToList();
+            IList<double> got = QuadraticFunction.Solve(a0, a1, a2).ToList();
             Assert.Equal(want.Length, got.Count);
             // There are no guarantees as to the order of values returned, so sort both sequences and compare items
             // at equal locations in the sorted sequence. Expect them all to match. We don't need to try any other
@@ -123,7 +99,7 @@ namespace EngineTests.calculus
             Array.Sort(want);
             int i = 0;
             var gotSorted = from c in got orderby c select c;
-            foreach(float f in gotSorted)
+            foreach(double f in gotSorted)
             {
                 Assert.Equal(want[i++], f, 6);
             }
